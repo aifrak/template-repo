65b16abfe5f6ec78e4ca2205d23d5a677939daaf
aifrak <aifrak@users.noreply.github.com>
2021-11-23 16:39:39 +0100

    build: Release and update `CHANGELOG.md` with `release-it` from workflow (#148)
    - Add `release-it` with its custom calver plugin
    - Move `ca-certificates` to let `release-it`'s dry run pass during `ci` workflow
    - Add `RELEASE.md`
    - Add `release` scope and remove `dev` scope
    - Add convention about Git workflow

---
+++ scripts/run/release.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+function release:help {
+  cat <<EOF
+
+Release commands:
+  release                     Cut a release, tag it and publish it to Github
+  release:ci                  Tag and cut a release from CI
+  release:dry-run             Simulate cutting a release without updating files or pushing to git
+  release:test                Dry run release, run tests for release and generate changelog temporary
+  changelog                   Generate/Update CHANGELOG.md without pushing to git
+  prerelease                  Cut a prerelease, tag with a suffix it and publish it to Github (default: dev)
+  - Usage: prerelease [dev|alpha|beta|rc]
+  release-it:custom:test      Run tests related to custom hooks and/or plugins for release-it
+EOF
+}
+
+function release {
+  npx release-it "${@}" --ci
+}
+
+function release:ci {
+  local modifier=${1:-""}
+
+  if [[ -n "${modifier}" ]]; then
+    prerelease "${modifier}"
+  else
+    release
+  fi
+}
+
+function release:dry-run {
+  release --dry-run --no-git.requireBranch --no-git.requireCleanWorkingDir
+}
+
+function release:test {
+  function reset_updated_files {
+    local updated_files=(
+      CHANGELOG.md
+      VERSION
+    )
+
+    git checkout -- "${updated_files[@]}"
+  }
+
+  release-it:custom:test
+  release:dry-run
+  changelog
+
+  trap '$(reset_updated_files)' EXIT
+}
+
+function prerelease {
+  local modifier=${1:-"dev"}
+  local allowed_modifiers="^(dev|alpha|beta|rc)$"
+
+  # Format and possible options depend on "release-it" plugins
+  if [[ ! "${modifier}" =~ ${allowed_modifiers} ]]; then
+    alert_die "Unknown version modifier. Accepted values: dev, alpha, beta, rc"
+  fi
+
+  release "calendar.${modifier}" --github.preRelease
+}
+
+function changelog {
+  release \
+    --no-git.commit \
+    --no-git.push \
+    --no-git.tag \
+    --no-git.requireBranch \
+    --no-git.requireCleanWorkingDir
+}
+
+function release-it:custom:test {
+  for path in ./scripts/release-it/tests/**/*.test.js; do
+    node "${path}"
+  done
+}
