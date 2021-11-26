b61bb99d4613b9e3ff07a765d6004c60b14a26b1
aifrak <aifrak@users.noreply.github.com>
2021-11-26 18:56:06 +0100

    chore(scripts): Test only hooks instead of whole changelog update (#154)

--- scripts/release-it/hooks/changelog/delete_header.sh
+++ scripts/release-it/hooks/changelog/delete_header.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+set -euo pipefail
 
 changelog="CHANGELOG.md"
 tag_cut_limit="<!-- changelog-header:end -->"
 
-if [ -f "${changelog}" ]; then
-  sed -i "1,/${tag_cut_limit}/d" "${changelog}"
-fi
+sed -i "1,/${tag_cut_limit}/d" "${changelog}"
