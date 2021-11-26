a972ff3cb44db2844548695f9a9426dba1c67757
aifrak <aifrak@users.noreply.github.com>
2021-11-26 17:54:06 +0000

    chore(scripts): Remove unnecessary condition

--- scripts/release-it/hooks/changelog/delete_header.sh
+++ scripts/release-it/hooks/changelog/delete_header.sh
@@ -5,6 +5,4 @@ set -euo pipefail
 changelog="CHANGELOG.md"
 tag_cut_limit="<!-- changelog-header:end -->"
 
-if [ -f "${changelog}" ]; then
-  sed -i "1,/${tag_cut_limit}/d" "${changelog}"
-fi
+sed -i "1,/${tag_cut_limit}/d" "${changelog}"
