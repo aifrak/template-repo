b0f84ae5ebd371ab375ac7273e4fa0bc58d0c307
aifrak <aifrak@users.noreply.github.com>
2022-05-04 13:20:17 +0000

    chore(release): Use ESM syntax for `calver-bumper.js`
    - Use `.cjs` extension for files using CommonJS

--- scripts/release-it/plugins/conventional-changelog/commit-groups-sort.cjs
+++ scripts/release-it/plugins/conventional-changelog/commit-groups-sort.cjs
@@ -5,7 +5,7 @@
 // https://github.com/conventional-changelog/conventional-changelog/blob/840d139c40a2c4e773794d794c34bc4dbfde27c2/packages/conventional-changelog-conventionalcommits/writer-opts.js#L160
 // but without the static array.
 
-const types = require('../../types');
+const types = require('../../types.cjs');
 
 function commitGroupsSort(a, b) {
   const groupA = a.title;
