ebe45e182d81cfa443d7be458de068e2b036949b
aifrak <aifrak@users.noreply.github.com>
2022-08-22 15:11:33 +0000

    chore: Rename variables and tasks

--- scripts/run/release.sh
+++ scripts/run/release.sh
@@ -7,10 +7,10 @@ Release commands:
   release                     Update CHANGELOG.md and version files, cut a release and publish to Github
   release:ci [MODIFIER]       Release from CI. If modifier given, it will prerelease.
   release:dry-run             Simulate cutting a release without updating files or pushing to git
-  test:release                Dry run release, run tests for release
-  test:release:hooks          Run tests for hooks during a release
   prerelease                  Cut a prerelease, tag with a suffix it and publish it to Github (default: dev)
   - Usage: prerelease [dev|alpha|beta|rc]
+  test:release                Dry run release, run tests for release
+  test:release:hooks          Run tests for hooks during a release
   test:release-it:custom      Run tests related to custom hooks and/or plugins for release-it
 EOF
 }
